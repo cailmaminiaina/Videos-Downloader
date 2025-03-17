1f7246bb63f664802a20da3e3cdf42bd38a5674c
Cail ll <132125510+cailmaminiaina@users.noreply.github.com>
2025-03-17 15:19:01 +0300

    Update download videos.py

--- download videos.py
+++ download videos.py
@@ -23,7 +23,7 @@ def telecharger_video(url):
     options = {
         'outtmpl': os.path.join(DOWNLOAD_FOLDER, '%(title)s.%(ext)s'),
         'format': 'best',
-        'cookiesfrombrowser': True
+        'cookiesfrombrowser': ['firefox']
     }
     with yt_dlp.YoutubeDL(options) as ydl:
         info_dict = ydl.extract_info(url, download=True)
